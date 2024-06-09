bc9b034dcc5338698f3a59214ed7fb0a6ecff9cf
David Andersen <git@davidandersen.us>
2024-06-08 20:22:34 -0700

    switched to skeleton

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,11 +1,59 @@
-import type { Config } from 'tailwindcss';
+import { join } from 'path'
+import type { Config } from 'tailwindcss'
+import { skeleton } from '@skeletonlabs/tw-plugin'
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
-		extend: {}
+		extend: {},
 	},
-
-	plugins: [require('@tailwindcss/typography')]
-} as Config;
+	plugins: [
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+					{
+						name: 'wintry',
+						enhancements: true,
+					},
+					{
+						name: 'modern',
+						enhancements: true,
+					},
+					{
+						name: 'hamlindigo',
+						enhancements: true,
+					},
+					{
+						name: 'rocket',
+						enhancements: true,
+					},
+					{
+						name: 'sahara',
+						enhancements: true,
+					},
+					{
+						name: 'gold-nouveau',
+						enhancements: true,
+					},
+					{
+						name: 'vintage',
+						enhancements: true,
+					},
+					{
+						name: 'seafoam',
+						enhancements: true,
+					},
+					{
+						name: 'crimson',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
+} satisfies Config;
